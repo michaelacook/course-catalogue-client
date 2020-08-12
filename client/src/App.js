04a7553ad0d1cc9f9d8dd290a4ebecefd651684a
Michael Cook <mcook0775@gmail.com>
2020-08-11 21:41:48 -0400

    Connect client and server, remove unneeded files

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const courses = fetch('http://localhost:5000/api/courses')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello, World!</h1>
   );
 }
 
