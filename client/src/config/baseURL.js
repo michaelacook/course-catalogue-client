123747c4703380e8ea69a0d58a05b1988f68b84f
Michael A. Cook <mcook0775@gmail.com>
2020-09-26 16:25:53 -0400

    Get ready for deployment

--- client/src/config/baseURL.js
+++ client/src/config/baseURL.js
@@ -1,6 +1,3 @@
 // get protocol, hostname and port and export a base url string
 
-const { protocol, hostname } = window.location
-const baseURL = `${protocol}//${hostname}:5000`
-
-export default baseURL
+export default "https://course-catalogue-restapi.herokuapp.com"
